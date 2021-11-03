80e3dcfb02dc18b12248592e6a0005ab0e159918
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-03 14:03:53 +0530

    Welcome Message

---
+++ LineComparisonCompute.java
@@ -0,0 +1,10 @@
+
+          public class LineComparisonCompute{
+
+                 public static void main(String[] args){
+  
+                         System.out.println("WELCOME TO THE LINE COMPARISON COMPUTAION PROGRAM");
+
+     }
+  }
+                           
