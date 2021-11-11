f3fce5d4c9e1ac64420e09d1f90a6fbb38434014
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-11 20:08:12 +0530

    Updated Line program by using OOPs concept

---
+++ src/com/lineprogram/MainLineCompare.java
@@ -0,0 +1,12 @@
+package com.lineprogram;
+
+public class MainLineCompare {
+	public static void main(String[] args) {
+		
+		
+		LineComparisonCompute object = new LineComparisonCompute();
+		object.LineCompute();
+	}
+}
+
+
